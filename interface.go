82c85ef494fc6f850994a5b84ed03b47654bbb46
Denny P Sam <blackstorm.denny@gmail.com>
2018-01-06 23:56:49 +0530

    basic function creation- file creation

--- interface.go
+++ interface.go
@@ -4,11 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 )
 
-func basicMainFunc() {
-	template := "package main \nimport 'fmt' \nfunc main(){ \n} "
-	fmt.Printf("%s", template)
+func basicMainFunc() string {
+	template := "package main \n \nimport 'fmt' \n \nfunc main(){ \n \n} "
+	return template
 }
 
 func main() {
@@ -18,6 +19,7 @@ func main() {
 	for i, item := range menuItems {
 		fmt.Printf("%d) %s\n", i+1, item)
 	}
+	fmt.Println("\nYour choice: ")
 
 	input := bufio.NewScanner(os.Stdin)
 	input.Scan()
@@ -25,11 +27,34 @@ func main() {
 
 	switch opt {
 	case "1":
-		fmt.Println("I will build you a template for ", menuItems[0])
+		fmt.Println("I will build you a template for ", menuItems[0], "\n")
 		basicMainFunc()
 	case "2":
-		fmt.Println("I will build you a template for ", menuItems[1])
+		fmt.Println("I will build you a template for ", menuItems[1], "\n")
 	case "3":
-		fmt.Println("I will build you a template for ", menuItems[2])
+		fmt.Println("I will build you a template for ", menuItems[2], "\n")
 	}
+
+	fmt.Println("Do you want to")
+	fmt.Println("1) Simply save the file")
+	fmt.Println("2) Open it in my favorite code editor")
+	input.Scan()
+	opt = input.Text()
+
+	switch opt {
+	case "1":
+		cmd := exec.Command("echo", "hey", "| basic.go")
+		fmt.Println(cmd.Run())
+	case "2":
+		fmt.Println("Please enter the command to open your favourite editor ")
+		input.Scan()
+		editor := input.Text()
+		cmd := exec.Command("sh", "-c", "echo %s >> basic.go", basicMainFunc())
+		fmt.Println(cmd.Run())
+
+		cmd = exec.Command(editor, "basic.go")
+		fmt.Println(cmd.Run())
+
+	}
+
 }
