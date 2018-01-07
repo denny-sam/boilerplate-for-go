1ba8f2a457505c2dd76e192b4d17d9474fbe4973
Denny P Sam <blackstorm.denny@gmail.com>
2018-01-07 16:00:14 +0530

    Bash interaction implemented

--- interface.go
+++ interface.go
@@ -7,13 +7,8 @@ import (
 	"os/exec"
 )
 
-func basicMainFunc() string {
-	template := "package main \n \nimport 'fmt' \n \nfunc main(){ \n \n} "
-	return template
-}
-
 func main() {
-	menuItems := []string{"Basic main function", "Hello world", "Basic server"}
+	menuItems := []string{"Basic main function", "Basic server"}
 
 	fmt.Println("\nHola, choose the boilerplate you want to start off with. Also if you have an editor of preference then specify that and we will open the code in it. Or else you can leave it there, in which case a file will be created in the current working directory with a name you specify. Happy Coding!\n")
 	for i, item := range menuItems {
@@ -23,37 +18,58 @@ func main() {
 
 	input := bufio.NewScanner(os.Stdin)
 	input.Scan()
-	opt := input.Text()
+	template := input.Text()
 
-	switch opt {
+	switch template {
 	case "1":
 		fmt.Println("I will build you a template for ", menuItems[0], "\n")
-		basicMainFunc()
 	case "2":
 		fmt.Println("I will build you a template for ", menuItems[1], "\n")
-	case "3":
-		fmt.Println("I will build you a template for ", menuItems[2], "\n")
 	}
 
 	fmt.Println("Do you want to")
 	fmt.Println("1) Simply save the file")
 	fmt.Println("2) Open it in my favorite code editor")
 	input.Scan()
-	opt = input.Text()
+	opt := input.Text()
 
 	switch opt {
 	case "1":
-		cmd := exec.Command("echo", "hey", "| basic.go")
-		fmt.Println(cmd.Run())
-	case "2":
-		fmt.Println("Please enter the command to open your favourite editor ")
-		input.Scan()
-		editor := input.Text()
-		cmd := exec.Command("sh", "-c", "echo %s >> basic.go", basicMainFunc())
-		fmt.Println(cmd.Run())
+		switch template {
 
-		cmd = exec.Command(editor, "basic.go")
-		fmt.Println(cmd.Run())
+		case "1":
+			err := exec.Command("sh", "-c", "cat basic_func.txt >template.go").Run()
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println("Saved your file as template.go")
+			}
+
+		default:
+			fmt.Println("Work in Progress...")
+
+		}
+
+	case "2":
+		switch template {
+
+		case "1":
+			fmt.Println("Please enter the command to open your favourite editor ")
+			input.Scan()
+			editor := input.Text()
+			err := exec.Command("sh", "-c", "cat basic_func.txt >template.go").Run()
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			err = exec.Command(editor, "template.go").Run()
+			if err != nil {
+				fmt.Println(err)
+			}
+
+		default:
+			fmt.Println("Work in Progress...")
+		}
 
 	}
 
